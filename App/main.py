934468b3c3f75db13692b7a0b86ba33f66d5d89f
lipskydan <lipsky@knu.ua>
2020-09-13 11:53:47 +0300

    add iris.data

--- App/main.py
+++ App/main.py
@@ -69,3 +69,13 @@ class Perceptron(object):
     def predict(self, X):
         """Return class label after unit step"""
         return np.where(self.net_input(X) >= 0.0, 1, -1)
+
+v1 = np.array([1, 2, 3])
+v2 = 0.5 * v1
+np.arccos(v1.dot(v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
+
+# ## Training a perceptron model on the Iris dataset
+
+# ...
+
+# ### Reading-in the Iris data
